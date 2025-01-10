ca8fe70cdee58fec304ee380b7f6f006cd03917c
aliasferno <aliasferno@gmail.com>
2025-01-09 20:14:22 -0500

    Inicio Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  txtN1: number = 0;
+  txtN2: number = 0;
+  txtResultado: number = 0;
+
   constructor() {}
+
+  sumar(){
+    const n1 = this.txtN1;
+    const n2 = this.txtN2;
+    const resultado = n1 + n2;
+    this.txtResultado = resultado;
+  }
+
+  restar(){
+    const n1 = this.txtN1;
+    const n2 = this.txtN2;
+    const resultado = n1 - n2;
+    this.txtResultado = resultado;
+  }
+
+  multiplicar(){
+    const n1 = this.txtN1;
+    const n2 = this.txtN2;
+    const resultado = n1 * n2;
+    this.txtResultado = resultado;
+  }
+
+  dividir(){
+    const n1 = this.txtN1;
+    const n2 = this.txtN2;
+    const resultado = n1 / n2;
+    this.txtResultado = resultado;
+  }
 
 }
